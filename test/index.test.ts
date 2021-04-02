0a3fff8df3ac2caa2ded3c56785c9ff5c112a735
genbs <gennaro.bs@gmail.com>
2021-04-02 19:50:53 +0200

    improve tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,61 @@
-import { ShapeBuffer } from '@urpflanze/core'
 import tap from 'tap'
 
-const SVGImporter = require('../dist/index').SVGImporter
+import { ShapeBuffer } from '@urpflanze/core'
+import { SVGImporter } from '../dist'
 
-const shape = SVGImporter.parse(`
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-    <rect width="200" height="200" fill="#ffffff" />
-    <path d="M50.000 50.000 L150.000 50.000 L150.000 150.000 L50.000 150.000 Z" fill="none" stroke="#000000" stroke-width="1" />
-</svg> 
-`) as ShapeBuffer
+const shape = SVGImporter.parse(
+	`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><path d="M0 0 L200 0 L200 200 L0 200 Z" /></svg> `,
+	0.1,
+	10
+) as ShapeBuffer
 
 shape.generate(0)
 
-tap.deepEqual(shape.getBuffer(), [50.0, 50.0, 150.0, 50.0, 150.0, 150.0, 50.0, 150.0], 'default')
+tap.deepEqual(
+	shape.getBounding(),
+	{
+		cx: 0,
+		cy: 0,
+		x: -10,
+		y: -10,
+		width: 20,
+		height: 20,
+	},
+	'check bounding'
+)
+
+tap.deepEqual(shape.getBuffer(), [-10, -10, 10, -10, 10, 10, -10, 10], 'check buffer')
+
+////
+
+const shape2 = SVGImporter.parse(
+	`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><g transform="scale(0.5 0.5)"><path d="M0 0 L200 0 L200 200 L0 200 Z" /></g></svg> `,
+	0.1,
+	10
+) as ShapeBuffer
+
+shape2.generate(0)
+console.log(shape2.getBuffer())
+tap.deepEqual(shape2.getBuffer(), [-10, -10, 0, -10, 0, 0, -10, 0], 'test group transform')
+
+////
+
+const shape3 = SVGImporter.parse(
+	`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><g transform="scale(0.5 0.5)"><path transform="translate(100 100) scale(0.5 0.5)" d="M0 0 L200 0 L200 200 L0 200 Z" /></g></svg> `,
+	0.1,
+	10
+) as ShapeBuffer
+
+shape3.generate(0)
+tap.deepEqual(shape3.getBuffer(), [-5, -5, 0, -5, 0, 0, -5, 0], 'test group and element transform')
+
+////
+
+const shape4 = SVGImporter.parse(
+	`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><path fill="red" opacity=".5" d="M0 0 L200 0 L200 200 L0 200 Z" /></svg> `,
+	0.1,
+	10
+) as ShapeBuffer
+
+shape4.generate(0)
+tap.deepEqual(shape4.drawer.fill, 'rgba(255, 0, 0, 0.5)', 'test fill color and opacity')
